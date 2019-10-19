7fe57a20f1650d7410918ac6bd9fef93201547de
spiciest hamster <pconnelly727@gmail.com>
2019-10-19 09:14:53 -0400

    logic for forms isn't correct, but the functionality for creating a user, logging in, and signing out works

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,7 +10,52 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-// making database connection 
+// making database and auth connection 
 const db = firebase.firestore();
 const auth = firebase.auth();
 
+// starting sign up information
+const signUpForm = document.querySelector(".sign-up-form");
+
+// adding an event listener on submission of the form
+// signUpForm.addEventListener("submit", function(event) {
+//     event.preventDefault();
+
+//     // grabbing user info
+//     const email = signUpForm["user-email-input"].value;
+//     const password = signUpForm["user-password-input"].value;
+
+//     // signing up the user (literally all you have to do... it's sick!)
+//     auth.createUserWithEmailAndPassword(email, password).then(function(credential) {
+//         console.log(credential.user);
+//     });
+// });
+
+// grabbing onto logout button
+const logoutButton = document.getElementById("logout-button");
+
+// logging user out of application
+logoutButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    auth.signOut().then(function() {
+        console.log("the user has logged out, and we should hide content");
+    });
+});
+
+// grabbing onto login button
+const loginButton = document.getElementById("login-button");
+
+signUpForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("we got in here");
+
+
+    // getting user credentials
+    const email = signUpForm["user-email-input"].value;
+    const password = signUpForm["user-password-input"].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(credential => {
+        console.log(credential.user);
+    });
+})
