c93672dc4ad9d46dce0d2f3636e54ce16d723291
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-10-19 12:47:55 -0400

    created itunes API loop funciton. Still need to hook to html and create source tags for image and song

---
+++ assets/javascript/main.js
@@ -0,0 +1,37 @@
+
+
+var tasteDiveArray = ["lizzo", "beyonce", "celine dion", "shania twain", "cher", "billy joel"];
+
+
+// /v1/artists/{id}
+function callItunesAPI(){
+    for(var i=0; i<tasteDiveArray.length; i++){
+        var artistNameFromArray = tasteDiveArray[i]
+        console.log(artistNameFromArray);
+        var queryURL ="https://cors-anywhere.herokuapp.com/"+ "https://itunes.apple.com/search?term=" + artistNameFromArray + "&limit=1";  
+   
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+      console.log(typeof response);
+             var result = JSON.parse(response).results;
+        console.log(result);
+        // console.log(response[0])
+        console.log(result[0].artworkUrl100);
+        console.log(result[0].artistName);
+        console.log(result[0].primaryGenreName);
+        console.log(result[0].trackName);      
+        console.log(result[0].previewUrl);
+        var artistName = $("<p>");
+        artistName.attr(result[0].artistName);
+        var artistImage = $("<img");
+        artistImage.attr("src", result[0].artworkUrl100);
+        artistImage.attr("alt", "artist");
+        $(".main-search-result-continer").append(artistName, artistImage);
+        
+    });
+
+}}
+callItunesAPI();
