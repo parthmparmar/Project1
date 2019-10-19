5298bbf7b468a2f944c905eed0c661ba00f05420
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-10-19 14:17:31 -0400

    updated main.js to include both funcitons and they are working together. Displaying image and name. Need to work on sound and song name

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,35 @@
+var searchValue = "drake";
+var searchType = "music";
+// key for tasteDiveKey
+var tasteDiveKey = "348203-ClassPro-YG3CBL5R";
+// search limit for the aip to result 
+var searchLimit = 9;
+var resultsArray = [];
 
+tasteDive(searchValue, searchType, tasteDiveKey, searchLimit);
 
-var tasteDiveArray = ["lizzo", "beyonce", "celine dion", "shania twain", "cher", "billy joel"];
+function tasteDive(value, type, key, limit) {
+    queryURL = "https://cors-anywhere.herokuapp.com/"+"https://tastedive.com/api/similar?q=" + value + "&type=" + type + "&k=" + key + "&limit=" + limit;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(results){
+        results.Similar.Results.forEach(function(item) {
+            resultsArray.push(item.Name);
+        });
+        console.log(resultsArray);
+        callItunesAPI();
+    });
 
+}
 
+// var tasteDiveArray = ["lizzo", "beyonce", "celine dion", "shania twain", "cher", "billy joel"];
+
+console.log(resultsArray);
 // /v1/artists/{id}
 function callItunesAPI(){
-    for(var i=0; i<tasteDiveArray.length; i++){
-        var artistNameFromArray = tasteDiveArray[i]
+    for(var i=0; i<resultsArray.length; i++){
+        var artistNameFromArray = resultsArray[i]
         console.log(artistNameFromArray);
         var queryURL ="https://cors-anywhere.herokuapp.com/"+ "https://itunes.apple.com/search?term=" + artistNameFromArray + "&limit=1";  
    
@@ -37,4 +60,4 @@ $.ajax({
     });
 
 }}
-callItunesAPI();
+// callItunesAPI();
