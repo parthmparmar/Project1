8b86a670a0aae6a1604e3c9b9a1cc71c938797f0
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-10-19 23:39:21 -0400

    created loading gif function.

---
+++ assets/javascript/loading.js
@@ -0,0 +1,6 @@
+setTimeout(loadingGIF, 1000);
+function loadingGIF(){
+    var loadingImage = $("<img>");
+    loadingImage.attr("src", "assets/images/loadng.gif");
+    $(".main-search-result-continer").append(loadingImage);
+}
